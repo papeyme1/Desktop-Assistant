f214db6c3bac298a8e67bd989156d045999e9c67
papeyme1 <jonahvergott@yahoo.com>
2026-07-19 20:17:51 -0400

    1. Assistant can now dynamically access function file and determine potential executables from user speech input.
    2. Timeout added to avoid infinite runtime.

--- assistant.py
+++ assistant.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr, pyttsx3, time
+from num2words import num2words
 
 active = True
 r = sr.Recognizer()
@@ -41,6 +42,7 @@ def record_text():
         pass
 
 def speak(sentence_string):
+    print(sentence_string)
     # Convert text to speech
     engine.say(str(sentence_string))
 
@@ -52,18 +54,51 @@ def onStart(name, location, length):
         print('Loading speech...')
         time.sleep(1.5)
 
+attempts = 1
 while(active):
     text = record_text()
 
     # Special cases
     if text == None:
+        attempts += 1
+        if attempts > 3:
+            print("No speech detected. If this doesn't seem right, please check your microphone.")
+            break
         continue
     if ("end" in text) or ("terminate" in text) or ("quit" in text) or ("kill" in text):
         active = False
         break
+    
+    # Number formatting
+    text = text.split(" ")
+    for word in text:
+        if word.isdigit():
+            print("Found number")
+            word = num2words(int(word))
+            print("Number: " + word)
+    text = " ".join(text)
+    print("after: " + text)
 
+    # Timeout handling
     print("Wrote text: "+ text)
+    attempts = 1
     engine.connect('started-word', onStart)
 
-    # Filler function used to verify speach-to-text accuracy
-    speak(text)
+    # Handles function calls from user inout
+    if ("execute" in text) or ("begin" in text) or ("start" in text) or ("use" in text):
+        with open("assistant_functions.py", "r", encoding="utf-8") as file:
+            potential_executables = []
+            matches = []
+
+            # Grabbing list
+            for line in file:
+                if "def" in line:
+                    potential_executables.append(line[4:(line.find("("))])
+
+            # Function matching
+            # print("Potential executables:", potential_executables)        
+            for function in potential_executables:
+                if function.replace("_", " ") in text:
+                    matches.append(function.replace("_", " "))
+            speech = ("Choose function: ", matches)
+            speak(speech)
---
+++ assistant_functions.py
@@ -0,0 +1,5 @@
+def test_one():
+    print("This is a test function that does nothing.")
+
+def search():
+    print("Terms have been searched.")
